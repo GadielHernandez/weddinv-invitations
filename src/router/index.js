a7825a3a15d26be4546148f74497c223de9f33fe
GadielHernandez <gadhdz96@gmail.com>
2021-10-28 14:19:49 -0500

    build: remove dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Invitation from '../views/Invitation.vue'
-import Dashboard from '../views/dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/dashboard/:wedding?',
-        name: 'Dashboard',
-        component: Dashboard,
-    },
     {
         path: '/:wedding?',
         name: 'Invitation',
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import guest from './modules/guest'
-import admin from './modules/admin'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        guest,
-        admin
+        guest
     },
 })
